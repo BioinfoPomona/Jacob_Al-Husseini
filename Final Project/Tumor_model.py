d4eb6b384f4c7113776bdd2b7e5b869168a06c19
jkalhusseini <jkae2018@mymail.pomona.edu>
2022-04-20 10:14:46 -0700

    Homework 3 and 5 Updates

---
+++ Final Project/Tumor_model.py
@@ -0,0 +1,18 @@
+import math
+import numpy as np
+import plotly as pl
+from dash import Dash, html, doc
+import plotly.express as px
+import pandas as pd
+
+from IPython.display import display
+w = widgets.IntSlider()
+display(w)
+slider_value = w.value
+
+app = Dash(braintumor)
+
+#Mathematical Model 
+Rp = 
+#Change in uniform growth
+dRp = (Rp/3)[(c1(g5+g6)-s*g1)/15]
